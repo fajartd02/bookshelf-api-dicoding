90323ff1c59a32851b29cdc838726b0d25da8b31
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-11-06 12:41:46 +0800

    adding server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const hapi = require('@hapi/hapi');
+
+async function init() {
+  const server = hapi.server({
+    host: 'localhost',
+    port: 5000,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server is running in ${server.info.uri}`);
+}
+
+init();
