f656bef9037a1fbfcf5f8696f1a15a613307c7c0
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-11-06 16:55:14 +0800

    create delete handler

--- src/handler.js
+++ src/handler.js
@@ -168,9 +168,29 @@ const updateBook = (req, h) => {
     }
 };
 
+function deleteBook(req, h) {
+    const { bookId } = req.params;
+
+    const index = books.findIndex((book) => book.id === bookId);
+    if (index === -1) {
+        return h.response({
+            status: 'fail',
+            message: 'Buku gagal dihapus. Id tidak ditemukan',
+        }).code(404);
+    }
+
+    books.splice(index, 1);
+
+    return h.response({
+        status: 'success',
+        message: 'Buku berhasil dihapus',
+    }).code(200);
+}
+
 module.exports = {
     addBookHandler,
     getAllBooks,
     getBook,
     updateBook,
+    deleteBook,
 };
